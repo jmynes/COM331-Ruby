ad3b11505f7cfd86d6dce4c95a51eabc6aaece71
Jordan Mynes <mynesjordan@gmail.com>
2018-01-17 16:12:51 -0600

    Clarified extra puts

--- ch8/8.1.rb
+++ ch8/8.1.rb
@@ -6,19 +6,20 @@ languages = ['English', 'Norwegian', 'Ruby']
 languages.each do |lang|
 	puts 'I love ' + lang + '!'
 	puts 'Don\'t you?'
-	puts			# I just think this helps readability.
+	puts	# I just think this helps readability, though it wasn't in the text.
 end
 
 puts 'And let\'s hear it for Java!'
 puts '<crickets chirp in the distance>'
 
-puts
+puts		# Separate output
+
 # Go-go-gadget-integer-method...
 3.times do
 	puts 'Hip-Hip-Hooray'
 end
 
-puts
+puts		# Separate output
 
 2.times do
 	puts '...you can say that again...'
