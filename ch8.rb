c598d9526aee25ccf9bd5ff8f8c2b6cb36204a2d
Jordan Mynes <mynesjordan@gmail.com>
2018-01-17 16:01:52 -0600

    8.1

--- ch8.rb
+++ ch8.rb
@@ -1,13 +1,14 @@
 # 8.0
+puts "\n\#\# 8.0 \#\#\n\n"	# Not sure why this requires a double \n at the end to work correctly
 
 names = ['Ada', 'Belle', 'Chris']
 
 puts names
-puts			# Print \n
+puts				# Print \n
 puts names[0]
 puts names[1]
 puts names[2]
-puts names[3]	# This is out of range
+puts names[3]		# This is out of range
 
 
 # Notably, this printed a newline instead of nil for me. Config?
@@ -22,4 +23,27 @@ puts other_peeps
 
 
 # 8.1
+puts "\n\#\# 8.1 \#\#\n\n"	
 
+languages = ['English', 'Norwegian', 'Ruby']
+
+languages.each do |lang|
+	puts 'I love ' + lang + '!'
+	puts 'Don\'t you?'
+	puts			# I just think this helps readability.
+end
+
+puts 'And let\'s hear it for Java!'
+puts '<crickets chirp in the distance>'
+
+puts
+# Go-go-gadget-integer-method...
+3.times do
+	puts 'Hip-Hip-Hooray'
+end
+
+puts
+
+2.times do
+	puts '...you can say that again...'
+end
