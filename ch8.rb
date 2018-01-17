66f7d88956f276367ac259875decc32686ec7168
Jordan Mynes <mynesjordan@gmail.com>
2018-01-17 15:54:39 -0600

    8.0 code examples

---
+++ ch8.rb
@@ -0,0 +1,25 @@
+# 8.0
+
+names = ['Ada', 'Belle', 'Chris']
+
+puts names
+puts			# Print \n
+puts names[0]
+puts names[1]
+puts names[2]
+puts names[3]	# This is out of range
+
+
+# Notably, this printed a newline instead of nil for me. Config?
+
+other_peeps = []
+other_peeps[3] = 'beebee Meaner'
+other_peeps[0] = 'Ah-ha'
+other_peeps[1] = 'beebee Seedee'
+other_peeps[0] = 'beebee Ah-ha'
+
+puts other_peeps
+
+
+# 8.1
+
