9a76f8e37afb6608ffb7e06d7d02b89dae560aae
Jordan Mynes <mynesjordan@gmail.com>
2018-01-17 17:15:51 -0600

    separated a and b

---
+++ ch8/8.3b.rb
@@ -0,0 +1,11 @@
+# 8.3b - Table of contents, revisited
+
+# Table of contents, revisited. Rewrite your table of contents 
+# program on page 36. Start the program with an array holding
+# all of the information for your table of contents (chapter
+# names, page numbers, and so on). Then print out the information
+# from the array in a beautifully formatted table of contents.
+
+puts "\n\#\# 8.3b - Table of contents, revisited \#\#\n\n"
+
+# Come back to this when ch.2-7 are caught up, it's a rewrite from chapter 6.2
