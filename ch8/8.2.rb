1a2892ecdd0ad2d089820eaf16c5f0f48e127ffa
Jordan Mynes <mynesjordan@gmail.com>
2018-01-17 16:13:01 -0600

    8.2

---
+++ ch8/8.2.rb
@@ -0,0 +1,30 @@
+# 8.2
+puts "\#\# 8.2 \#\#\n\n"	
+
+foods = ['artichoke', 'brioche', 'caramel']
+
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join(', ')
+puts
+puts foods.join(' :) ') + ' 8)'
+
+200.times do
+	puts []
+end
+
+puts	# Separate code
+
+favorites = []
+favorites.push 'raindrops on roses'
+favorites.push 'whiskey on kittens'
+
+puts favorites[0]
+puts favorites.last
+puts favorites.length
+
+puts favorites.pop
+puts favorites
+puts favorites.length
