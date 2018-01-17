6b272b38d7d3531ec25dc69681ed71e552db4b3a
Jordan Mynes <mynesjordan@gmail.com>
2018-01-17 17:25:37 -0600

    Chapter 3 done

---
+++ ch3/3.0-3.3.rb
@@ -0,0 +1,51 @@
+# 3.0-3.3
+puts "\#\# 3.0-3.3 \#\#\n\n"	# Not sure why this requires a double \n at the end to work correctly
+
+puts 'Hello, world'
+puts ''
+puts 'Good-bye.'
+
+puts	# Separation of output
+
+# 3.1
+puts 'I like' + 'apple pie'
+
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+
+puts	# Separation of output
+
+puts 'blink ' * 4
+
+puts	# Separation of output
+
+# 3.2
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts	# Separation of output
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts	# Separation of output
+
+# 3.3
+# Below lines are incorrect syntax!
+#puts '12' + 12
+#puts '2' * '5'
+#puts 'Betty' + 12
+#puts 'Fred' * 'John'
+#puts 'You're swell!'
+
+# Escape certain characters!
+puts 'You\'re swell!'
+
+puts	# Separation of output
+
+puts 'You\'re swell!'
+puts 'backslack at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
