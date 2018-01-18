1b0751967056f195d4acfa25bf5a0d4a0c43af90
Jordan Mynes <mynesjordan@gmail.com>
2018-01-17 18:13:06 -0600

    Single responsibility principle, separate speed reporting into own function

--- inClass/car/car.rb
+++ inClass/car/car.rb
@@ -5,15 +5,23 @@ class Car
     @speed = 0
   end
 
-  def accelerate
+  #car.gas
+  def gas
     # CANNOT @speed++ in Ruby!
     @speed +=1
-    puts "You are going #{@speed} miles per hour"
+    how_fast
   end
 
+  #car.brake
   def brake # rather than wording it as decelerate, let's call it brake
     # CANNOT @speed-- in Ruby!
     @speed -=1
+    how_fast
+  end
+
+  # Single responsibility principle, separate this log out of the gas/brake
+  car.how_fast
+  def how_fast
     puts "You are going #{@speed} miles per hour"
   end
 end
