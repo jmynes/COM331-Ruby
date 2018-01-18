3b758239ec39806a0b5c0057928069370581f426
Jordan Mynes <mynesjordan@gmail.com>
2018-01-17 18:19:54 -0600

    Brake can no longer result in negative speed

--- inClass/car/car.rb
+++ inClass/car/car.rb
@@ -15,12 +15,14 @@ class Car
   #car.brake
   def brake # rather than wording it as decelerate, let's call it brake
     # CANNOT @speed-- in Ruby!
-    @speed -=1
+    if @speed > 0
+      @speed -=1
+    end
     how_fast
   end
 
   # Single responsibility principle, separate this log out of the gas/brake
-  car.how_fast
+  #car.how_fast
   def how_fast
     puts "You are going #{@speed} miles per hour"
   end
