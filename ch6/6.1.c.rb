5f988546e9fa0fbc8edd1f9796ac1e3c3a856922
Jordan Mynes <mynesjordan@gmail.com>
2018-01-21 17:54:17 -0600

    Added note about suggested fix

--- ch6/6.1.c.rb
+++ ch6/6.1.c.rb
@@ -1,6 +1,8 @@
 # 6.1.c
 
-
+# I could probably find a way to figure out the amount of letters
+# in a string stripping its spaces, but the book didn't ask me
+# to and that's much more complicated than what this chapter requests.
 
 puts 'Hello there, and what\'s your first name?'
 first = gets.chomp
