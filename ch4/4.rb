2e3258b74c25ccd5af83798e553d41a85a3d7b2d
Jordan Mynes <mynesjordan@gmail.com>
2018-01-21 16:53:41 -0600

    chapter 4 I guess

---
+++ ch4/4.rb
@@ -0,0 +1,30 @@
+# 4
+
+my_string = '...you can say that again...'
+
+puts my_string
+puts my_string
+
+puts	# Separation of output
+
+name = 'Anya Christina Emanuella Jenkins Harris'
+puts ' My name is ' + name + '.'
+puts 'Wow! ' + name
+puts 'is a really long name!'
+
+puts	# Separation of output
+
+my_own_var = 'just another ' + 'string'
+puts my_own_var
+
+my_own_var = 5 * (1+2)
+puts my_own_var
+
+puts	# Separation of output
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts	# Separation of output
