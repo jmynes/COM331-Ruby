9f654df97dc505468422b6f89961d7315b15b226
Jordan Mynes <mynesjordan@gmail.com>
2018-01-21 18:30:08 -0600

    Broke into sub files

--- ch6/6.3-6.4.rb
+++ ch6/6.3-6.4.rb
@@ -1,5 +1,3 @@
-# 6.3-6.5
-
 # 6.3-6.4
 
 puts 5**2
@@ -12,22 +10,3 @@ puts	# Separation of output
 
 puts (5-2).abs
 puts (2-5).abs
-
-# 6.5
-puts	# Separation of output
-
-puts rand
-puts rand
-puts rand
-puts(rand(100))
-puts(rand(100))
-puts(rand(100))
-puts(rand(1))
-puts(rand(1))
-puts(rand(1))
-puts(rand(9999999999999999999999999999999999999))
-puts('The weatherman said there is a')
-# Sublimte doesn't quite like the '% c' here, marking them in purple
-# with syntax highlighting (the color of numbers), rather than yellow
-puts(rand(101).to_s + '% chance of rain')
-puts('but you can never trust a weatherman.')
---
+++ ch6/6.5.a.rb
@@ -0,0 +1,17 @@
+# 6.5a
+
+puts rand
+puts rand
+puts rand
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(1))
+puts(rand(1))
+puts(rand(1))
+puts(rand(9999999999999999999999999999999999999))
+puts('The weatherman said there is a')
+# Sublimte doesn't quite like the '% c' here, marking them in purple
+# with syntax highlighting (the color of numbers), rather than yellow
+puts(rand(101).to_s + '% chance of rain')
+puts('but you can never trust a weatherman.')
