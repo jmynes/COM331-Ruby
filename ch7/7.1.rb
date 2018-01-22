ba21d18ffc6370fee825e64a10ce4784df20258e
Jordan Mynes <mynesjordan@gmail.com>
2018-01-21 18:40:55 -0600

    7.1

---
+++ ch7/7.1.rb
@@ -0,0 +1,30 @@
+#7.1
+
+puts 1 > 2
+puts 1 < 2
+
+puts	# Separation of output
+
+puts 5 >= 5
+puts 5 <= 4
+
+puts	# Separation of output
+
+puts 1 == 1
+puts 2!=1
+
+puts	# Separation of output
+
+puts 'cat' < 'dog'
+
+puts	# Separation of output
+
+# Note that 'Xander' comes before 'bug lady' because of capital X
+# Use down/upcase or capitalize to prevent such an issue if sorting
+puts 'bug lady'          < 'Xander'
+puts 'bug lady'.downcase < 'Xander'.downcase
+
+puts	# Separation of output
+
+puts 2 < 10
+puts '2' < '10'
