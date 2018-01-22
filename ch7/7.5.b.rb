bf85f4a4f6c17800fb99973ea1f5a5798eacdc43
Jordan Mynes <mynesjordan@gmail.com>
2018-01-22 01:00:45 -0600

    Solved the mystery of the time travelling Grandma

--- ch7/7.5.b.rb
+++ ch7/7.5.b.rb
@@ -8,12 +8,9 @@ while true
   if response != response.upcase #&& response != ''
     puts 'HUH?! SPEAK UP, SONNY!'
   else
-    # Would be better with a floor and ceiling, such as prng.rand(1740..1940).to_s
-    # But I get:
-    # 7.5.b.rb:12:in `<main>': undefined local variable or method `prng' for main:Object (NameError)
-    
-    # This returns years that did not happen yet but at least have the right amount of digits.
-    puts 'NO, NOT SINCE ' +rand(10000).to_s + '!'
+    prng = Random.new
+    # Potentially one old Grandma!
+    puts 'NO, NOT SINCE ' + prng.rand(1740..1940).to_s + '!'
     break
   end
 end
