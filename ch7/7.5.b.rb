ce3a6d932f6674fc42238c20208e1bbfd792a705
Jordan Mynes <mynesjordan@gmail.com>
2018-01-22 01:11:18 -0600

    Variable clarification

--- ch7/7.5.b.rb
+++ ch7/7.5.b.rb
@@ -3,19 +3,19 @@
 # This would be greatly improved with methods
 # NOT at all DRY
 
-attempt_to_leave = 0
+attempts_to_leave = 0
 
-while attempt_to_leave < 3
+while attempts_to_leave < 3
   puts 'What would you like to say to Grandma?'
   response = gets.chomp
 
   # currently the commented out code does not prevent empty responses
   if response != response.upcase #&& response != ''
     puts 'HUH?! SPEAK UP, SONNY!'
-    attempt_to_leave = 0
+    attempts_to_leave = 0
   elsif response == 'BYE'
-    attempt_to_leave += 1
-    if attempt_to_leave < 3
+    attempts_to_leave += 1
+    if attempts_to_leave < 3
       puts 'HUH?! WHAT DID YOU SAY?!'
     end
   else
@@ -23,6 +23,6 @@ while attempt_to_leave < 3
     # Potentially one old Grandma!
     # I could also do this with rand(numberhere) and add it to some base
     puts 'NO, NOT SINCE ' + prng.rand(1740..1940).to_s + '!'
-    attempt_to_leave = 0
+    attempts_to_leave = 0
   end
 end
