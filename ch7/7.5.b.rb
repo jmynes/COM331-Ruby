8c982fd4bfb953c3fac3bdddbec067f2d917fb14
Jordan Mynes <mynesjordan@gmail.com>
2018-01-22 00:52:32 -0600

    Implemented grandma with missing features

--- ch7/7.5.b.rb
+++ ch7/7.5.b.rb
@@ -1,2 +1,14 @@
 #7.5b
 
+while true
+  puts 'What would you like to say to Grandma?'
+  response = gets.chomp
+
+  # currently the commented out code does not prevent empty responses
+  if response != response.upcase #&& response != ''
+    puts 'HUH?! SPEAK UP, SONNY!'
+  else
+    puts 'NO, NOT SINCE 1938!'
+    break
+  end
+end
