375ef08f1967621eb483f01bdb40c548abaf00db
Jordan Mynes <mynesjordan@gmail.com>
2018-01-22 01:19:17 -0600

    Leap year checker

---
+++ ch7/7.5.c.rb
@@ -0,0 +1,18 @@
+#7.5c
+
+# Does not check if years are valid or in correct order
+
+puts 'What year do you wish to start with?'
+start_year = gets.chomp.to_i
+
+puts 'What year do you wish to end with?'
+end_year = gets.chomp.to_i
+
+current_year = start_year
+
+while current_year < end_year
+  if (current_year % 400 == 0) || (current_year % 4 == 0) && !(current_year % 100 == 0)
+    puts "found " + current_year.to_s
+  end
+  current_year += 1
+end
