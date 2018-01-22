62b59b121b5ceba1f673b4e446711bfec8ba528d
Jordan Mynes <mynesjordan@gmail.com>
2018-01-22 00:29:26 -0600

    Working beers on the wall, doesn't conjugate for 1 and 0 beers

--- ch7/7.5.rb
+++ ch7/7.5.rb
@@ -1,2 +1,10 @@
 #7.5
 
+bottles_of_beer = 99
+while bottles_of_beer >= 1
+  puts bottles_of_beer.to_s + ' bottles of beer on the wall, ' + bottles_of_beer.to_s + ' bottles of beer!'
+  bottles_of_beer -= 1
+  puts 'Take one down, pass it around'
+  puts bottles_of_beer.to_s + ' bottles of beer on the wall!'
+  puts
+end
