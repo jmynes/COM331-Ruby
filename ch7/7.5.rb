dd61c5a2ab96685abff22401ecce8326865946cd
Jordan Mynes <mynesjordan@gmail.com>
2018-01-22 00:34:48 -0600

    Conjugation of bottle/s works for all non-zero integers

--- ch7/7.5.rb
+++ ch7/7.5.rb
@@ -1,10 +1,18 @@
 #7.5
 
 bottles_of_beer = 99
+bottle_unit = "bottles"
+
 while bottles_of_beer >= 1
-  puts bottles_of_beer.to_s + ' bottles of beer on the wall, ' + bottles_of_beer.to_s + ' bottles of beer!'
+  if bottles_of_beer != 1
+    bottle_unit = "bottles"
+  else
+    bottle_unit = "bottle"
+  end
+
+  puts bottles_of_beer.to_s + ' ' + bottle_unit + ' of beer on the wall, ' + bottles_of_beer.to_s + ' ' + bottle_unit + ' of beer!'
   bottles_of_beer -= 1
   puts 'Take one down, pass it around'
-  puts bottles_of_beer.to_s + ' bottles of beer on the wall!'
+  puts bottles_of_beer.to_s + ' ' + bottle_unit + ' of beer on the wall!'
   puts
 end
