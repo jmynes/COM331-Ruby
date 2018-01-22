a700b9fd86788f9296af2109c14e1e2577be2d33
Jordan Mynes <mynesjordan@gmail.com>
2018-01-22 00:58:59 -0600

    Kind of works, years are wonky

--- ch7/7.5.b.rb
+++ ch7/7.5.b.rb
@@ -8,7 +8,12 @@ while true
   if response != response.upcase #&& response != ''
     puts 'HUH?! SPEAK UP, SONNY!'
   else
-    puts 'NO, NOT SINCE 1938!'
+    # Would be better with a floor and ceiling, such as prng.rand(1740..1940).to_s
+    # But I get:
+    # 7.5.b.rb:12:in `<main>': undefined local variable or method `prng' for main:Object (NameError)
+    
+    # This returns years that did not happen yet but at least have the right amount of digits.
+    puts 'NO, NOT SINCE ' +rand(10000).to_s + '!'
     break
   end
 end
