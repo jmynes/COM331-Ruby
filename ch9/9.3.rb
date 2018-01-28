62de6150795bb626307bfb6b0ea9e7bb8591e23f
Jordan Mynes <mynesjordan@gmail.com>
2018-01-28 14:53:36 -0600

    More examples of variables and scope

--- ch9/9.3.rb
+++ ch9/9.3.rb
@@ -1,1 +1,26 @@
 # 9.3
+
+puts "\n\#\# 9.3 \#\#\n\n"
+
+def square x
+  puts(x * x)
+end
+
+x = 5
+square x
+
+puts # Separation of output
+
+my_number = 5
+square my_number
+
+puts # Separation of output
+
+x = 10
+my_number = x / 2
+square my_number
+
+# Don't do this.
+def square liauwechygfakcuewhalcufe
+  liauwechygfakcuewhalcufe * liauwechygfakcuewhalcufe
+end
