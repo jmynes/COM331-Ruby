e793a0b711c7358825410d80aaccad57cb033455
Jordan Mynes <mynesjordan@gmail.com>
2018-01-28 14:41:16 -0600

    Demonstration of arguments

---
+++ ch9/9.1.rb
@@ -0,0 +1,12 @@
+# 9.1
+
+def say_moo number_of_moos
+  puts 'mooooooo...'*number_of_moos
+end
+
+say_moo 3
+puts 'coin-coin'
+
+#  This last line should give an error
+#  because the parameter is missing...
+say_moo
