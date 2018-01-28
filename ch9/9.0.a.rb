289acf31719886851df30ade4037cb6a0e3cc4b7
Jordan Mynes <mynesjordan@gmail.com>
2018-01-28 14:38:49 -0600

    Fixed issue with bed_wet setting

--- ch9/9.0.a.rb
+++ ch9/9.0.a.rb
@@ -1,4 +1,4 @@
-# 9.0
+# 9.0a
 
 # Pine's example of ugly code, lots of repetition.
 
@@ -38,6 +38,11 @@ while true
   puts 'Do you wet the bed?'
   answer = gets.chomp.downcase
   if (answer == 'yes' || answer == 'no')
+    if (answer == 'yes')
+      wets_bed = true
+    else
+      wets_bed = false
+    end
     break
   else
     puts 'Please answer "yes" or "no".'
