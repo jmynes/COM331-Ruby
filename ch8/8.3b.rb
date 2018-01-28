e71551f59001e19dbf4ded3884fb96c85eed6fb6
Jordan Mynes <mynesjordan@gmail.com>
2018-01-28 14:26:50 -0600

    Implemented arrays for table of contents

--- ch8/8.3b.rb
+++ ch8/8.3b.rb
@@ -8,4 +8,20 @@
 
 puts "\n\#\# 8.3b - Table of contents, revisited \#\#\n\n"
 
-# Come back to this when ch.2-7 are caught up, it's a rewrite from chapter 6.2
+# 6.2.b revisited
+
+# I am still certain there's a better way to do this than
+# separate line_width and manual spaces in variables.
+# Some kind of printf equivalent would likely do it.
+
+ch = ["Chapter 1:   Getting Started", "Chapter 2:   Numbers", "Chapter 3:   Letters"]
+pg = ["Page   1", "Page   2", "Page   3"]
+
+header = 'Table of Contents'
+line_width = 50
+
+puts(header.center(line_width))
+puts
+puts(ch[0].ljust(line_width/2) + pg[0].rjust(line_width/2.25))
+puts(ch[1].ljust(line_width/2) + pg[1].rjust(line_width/2))
+puts(ch[2].ljust(line_width/2) + pg[2].rjust(line_width/2))
