26b6985e9d6652d3812b1e163dfedb8ceedebfe2
Jordan Mynes <mynesjordan@gmail.com>
2018-01-28 18:44:44 -0600

    Recursion examples

---
+++ ch10/10.1.b.rb
@@ -0,0 +1,17 @@
+# 10.1b
+puts "\n\#\# 10.1b\#\#\n\n"
+
+def factorial num
+  if num < 0
+    return 'You can\'t take the factorial of a negative number!'
+  end
+
+  if num <= 1
+    1
+  else
+    num * factorial(num-1)
+  end
+end
+
+puts factorial(3)
+puts factorial(30)
