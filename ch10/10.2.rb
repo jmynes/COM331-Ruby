eab06bc6d88da5e9b74dc81309dd3a7a8f8db6a7
Jordan Mynes <mynesjordan@gmail.com>
2018-01-31 00:40:45 -0600

    Found better solution

--- ch10/10.2.rb
+++ ch10/10.2.rb
@@ -2,18 +2,14 @@
 puts "\n\#\# 10.2 \#\#\n\n"
 unsorted_wordlist = []
 
-#def sort some_array  #  This "wraps" recursive_sort.
-#  recursive_sort some_array, []
-#end
-
 puts 'Enter words separated by enter.'
 puts 'An empty line will return a sorted list and exit the program.'
 puts
-
 
 while (entry = gets.chomp) != ''
   unsorted_wordlist.push entry
 end
 
-puts unsorted_wordlist.sort
-
+# Wouldn't this be even better than recursive sort?
+# Cleverness due to: https://stackoverflow.com/a/28414805
+puts unsorted_wordlist.sort_by(&:swapcase)
