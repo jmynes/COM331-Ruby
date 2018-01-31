e33d6f5276f17178777acf108177b3d7146973a5
Jordan Mynes <mynesjordan@gmail.com>
2018-01-31 03:16:33 -0600

    Implemented methods

--- ch10/10.5.c.rb
+++ ch10/10.5.c.rb
@@ -6,25 +6,35 @@ puts "\n\#\# 10.5.c - 99 Bottles of Beer on the Wall: Reloaded\#\#\n\n"
 # This works, but could be improved to read and execute better.
 # Lots of ifs in this while loop, I believe some are redundant.
 
-bottles_of_beer = 99
-bottle_unit = "bottles"
+$bottles_of_beer = 99
+$bottle_unit = "bottles"
 
-while bottles_of_beer >= 1
-  if bottles_of_beer != 1
-    bottle_unit = "bottles"
-  else
-    bottle_unit = "bottle"
-  end
-
-  puts bottles_of_beer.to_s + ' ' + bottle_unit + ' of beer on the wall, ' + bottles_of_beer.to_s + ' ' + bottle_unit + ' of beer!'
-  bottles_of_beer -= 1
+def take_one_down
+  $bottles_of_beer -= 1
   puts 'Take one down, pass it around'
-  
-  if bottles_of_beer == 0 
-    bottle_unit = "bottles"
-  end
+end
 
-  puts bottles_of_beer.to_s + ' ' + bottle_unit + ' of beer on the wall!'
+def refrain
+  #if $bottles_of_beer != 1
+    #$bottle_unit = "bottles"
+  #else
+    #$bottle_unit = "bottle"
+  #end
+
+  #if $bottles_of_beer == 0 
+    #$bottle_unit = "bottles"
+  #end
+
+  puts $bottles_of_beer.to_s + ' ' + $bottle_unit + ' of beer on the wall, ' + $bottles_of_beer.to_s + ' ' + $bottle_unit + ' of beer!'
+  puts
+  take_one_down
+end
+
+while $bottles_of_beer >= 1
+  refrain
+  puts $bottles_of_beer.to_s + ' ' + $bottle_unit + ' of beer on the wall!'
+  puts
+  puts "*Deep breath*"
   puts
 end
 
