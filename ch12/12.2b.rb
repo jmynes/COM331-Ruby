1539fa1b4c8e252b03db9ed6ecc010cce77dfddb
Jordan Mynes <mynesjordan@gmail.com>
2018-02-02 21:37:25 -0600

    Birthday punches calculator

---
+++ ch12/12.2b.rb
@@ -0,0 +1,69 @@
+# 12.2b
+puts "\n\#\# 12.2b - Happy Birthday \#\#\n\n"
+
+# Initialize today
+today = Time.now
+
+
+# Ideally we would use something closer to this probably
+# puts "Enter your birthdate in the format dd/mm/yyyy:"
+# birthdate = gets.chomp.to_i.split("/")
+# ...
+
+# Prompt user for birth year
+puts "Enter your birth year:"
+year = gets.chomp.to_i
+
+# Prompt user for birth month
+puts "Enter your birth month:"
+month = gets.chomp.to_i
+
+# Prompt user for birth day
+puts "Enter your birth day"
+day = gets.chomp.to_i
+
+age = today.year - year
+
+if today.month - month < 0
+  age -= 1
+elsif today.day - day < 0
+  age -= 1
+end
+
+#born = Time.local(year, month, day)
+puts
+#puts "You were born on #{born}?"
+puts "That makes you #{age} years old!"
+puts "Time for birthday punches!"
+puts
+
+punch = 0
+age.times{
+  punch += 1
+  if punch > 1
+    puts "*#{punch} punches in the arm*"
+  else
+    puts "*#{punch} punch in the arm*"
+  end
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#born = Time.local(1994, 04, 20, 13, 37)
+#one_billion = born + 1000000000
+
+
+
+#puts "I was allegedly born on #{born}"
+#puts "Theoretically making me one billion seconds old on: #{one_billion}"
+
