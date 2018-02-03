2234ae6f18bb3505609eaee5a95ba47587b23e14
Jordan Mynes <mynesjordan@gmail.com>
2018-02-02 23:30:34 -0600

    Added documentation

--- ch12/12.6.b.rb
+++ ch12/12.6.b.rb
@@ -1,12 +1,22 @@
 # 12.6a
 puts "\n\#\# 12.6b - Birthday Helper \#\#\n\n"
 
+#  Initialize a hash named birthdays
 birthdays = {}
+
+#  Open the file birthdays.txt
 File.read('birthdays.txt').each_line do|entry|
+  #  Just to standardize things for later
   entry = entry.chomp.downcase
 
+  #  Regex check for name, just assuming it's valid
+  # from the beginning of the line until a comma
   name = entry.match(/([^,]+)/)[0].to_s
+
+  #  Regex check for date, starting with a word and 
+  #  up through a comma preceding a number (year) til end of line
   date = entry.match(/\w+\s+\d+,.+/).to_s
 
+  #  Put it in the birthday hash!
   birthdays[name] = date
 end
