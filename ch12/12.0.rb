e0a4a7fdc0f2ac2b64aac1053525a68d96ae1644
Jordan Mynes <mynesjordan@gmail.com>
2018-02-02 21:01:49 -0600

    playing with time

---
+++ ch12/12.0.rb
@@ -0,0 +1,10 @@
+# 12.0
+puts "\n\#\# 12.0 \#\#\n\n"
+
+alpha = Array.new   + [12345]
+beta  = Array.new   + ['hello']
+karma = Time.new
+
+puts "alpha = #{alpha}"
+puts "beta  = #{beta}"
+puts "karma = #{karma}"
---
+++ ch12/12.1.rb
@@ -0,0 +1,17 @@
+# 12.1
+puts "\n\#\# 12.1 \#\#\n\n"
+
+time   = Time.new    #  The moment we ran this code.
+time2  = time + 60   #  One minute later.
+
+puts time
+puts time2
+
+puts  # Separation of output
+
+puts Time.local(2000, 1, 1)           # YSK.
+puts Time.local(1976, 8, 3, 13, 31)   # When the author was born.
+
+puts  # Separation of output
+
+puts Time.gm(1955, 11, 5)             # A red-letter day
