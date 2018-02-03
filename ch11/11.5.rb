808b92f9717ffd76f97cc244ad3da78071f67b2f
Jordan Mynes <mynesjordan@gmail.com>
2018-02-02 19:05:54 -0600

    Newlines and quotes

---
+++ ch11/11.5.rb
@@ -0,0 +1,35 @@
+# 11.5
+puts "\n\#\# 11.5 \#\#\n\n"
+
+buffy_quote_1 = '\'Kiss rocks\'?
+Why would anyone want to kiss...
+Oh, wait. I get it.'
+
+buffy_quote_2 = "'Kiss rocks'?\n" +
+                "Why would anyone want to kiss...\n" +
+                "Oh, wait. I get it."
+
+puts buffy_quote_1
+puts
+puts(buffy_quote_1 == buffy_quote_2)
+
+puts  # Separation of output
+
+puts "3...\n2...\n1...\nHAPPY NEW YEAR!"
+puts '3...\n2...\n1...\nHAPPY NEW YEAR!'
+
+puts  # Separation of output
+
+puts 'single (\') and double (") quotes'
+puts "single (') and double (\") quotes"
+
+puts  # Separation of output
+
+name = 'Luke'
+zip  = 90210
+
+puts "Name = #{name}, Zipcode = #{zip}"
+
+puts  # Separation of output
+
+puts "#{2 * 10**4 + 1} Leagues Under the Sea, THE REVENGE!"
