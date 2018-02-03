e4c43db3dcd4486e9d1da7e9994bb812efd06261
Jordan Mynes <mynesjordan@gmail.com>
2018-02-02 23:38:20 -0600

    Class fun

---
+++ ch12/12.7.rb
@@ -0,0 +1,12 @@
+# 12.7
+puts "\n\#\# 12.7 - Class Fun \#\#\n\n"
+
+puts(42.class)
+puts("I'll have mayonnaise on mine!".class)
+puts(Time.new.class)    #  No shocker here.
+puts(Time.class)        #  A little more interesting...
+puts(String.class)      #  Yeah, OK...
+
+#  Hold oyur breath through the tunnel, boys and girls!
+puts(Class.class)
+#  <gasp!>
