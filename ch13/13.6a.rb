285e706e229bc02824c75b9838b3027b808d65f6
Jordan Mynes <mynesjordan@gmail.com>
2018-02-03 01:08:39 -0600

    Tree skeleton

---
+++ ch13/13.6a.rb
@@ -0,0 +1,32 @@
+# 13.6a 
+puts "\n\#\# 13.6a - Orange Tree \#\#\n\n"
+
+class OrangeTree
+
+  # IT'S ALIVE!!
+  def initialize
+    @height       = 0
+    @orange_count = 0
+    @alive        = true
+  end
+  
+  def height
+
+  end
+
+  def one_year_passes
+    #age+=1
+  end
+
+  def produce_fruit
+
+  end
+
+  def pick_an_orange
+
+  end
+
+  def count_the_oranges
+
+  end
+end
